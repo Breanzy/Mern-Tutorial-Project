2a12dbf9b7f3f98930b982e452bd3a0260381a4d
Brean Julius Carbonilla <julius.carbonilla@gmail.com>
2024-09-05 18:58:03 +1000

    Initial Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>App</div>
+  )
+}
+
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
+
+
+import React from 'react'
+
+function App() {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
