2a3bc13e257d9b8ab44e59c72c9efb9b1dc8290a
Brean Julius Carbonilla <julius.carbonilla@gmail.com>
2024-09-10 09:10:58 +1000

    Create Signup api Route with hashed password

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,3 @@
 export const test = ("/test", (req, res) => {
-    res.json({ message: "API is working properly" });
+    res.json({ message: "API is working properlyt" });
 });
--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRoutes from './routes/user.route.js'
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -12,8 +13,11 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log("listening on on port 3000");
 });
 
-app.use('/api/user', userRoutes)
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
