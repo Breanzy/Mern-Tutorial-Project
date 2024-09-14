659c170abac63c7ab7dc6f74e0cc01965f8dea26
Brean Julius Carbonilla <julius.carbonilla@gmail.com>
2024-09-14 11:09:47 +1000

    Add redux toolkit

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,13 +1,18 @@
 import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+    signInStart,
+    signInSuccess,
+    signInFailure,
+} from "../redux/user/userSlice";
 
 export default function SignIn() {
     const [formData, setFormData] = useState({});
-    const [errorMessage, setErrorMessage] = useState(null);
-    const [loading, setLoading] = useState(false);
+    const {loading, error: errorMessage} = useSelector(state => state.user)
+    const dispatch = useDispatch();
     const navigate = useNavigate();
-
     const handleChange = (e) => {
         setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
     };
@@ -15,8 +20,7 @@ export default function SignIn() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            setLoading(true);
-            setErrorMessage(null);
+            dispatch(signInStart());
             const res = await fetch("/api/auth/signin", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
@@ -24,15 +28,14 @@ export default function SignIn() {
             });
             const data = await res.json();
             if (data.success === false) {
-                return setErrorMessage(data.message);
+                dispatch(signInFailure(data.message));
             }
             if (res.ok) {
+                dispatch(signInSuccess(data));
                 navigate("/");
             }
         } catch (error) {
-            setErrorMessage(error.message);
-        } finally {
-            setLoading(false);
+            dispatch(signInFailure(data.message));
         }
     };
 
