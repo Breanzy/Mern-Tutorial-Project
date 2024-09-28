b4dc4399297f5bc70c1679879dbc128f9ede8d11
Brean Julius Carbonilla <julius.carbonilla@gmail.com>
2024-09-28 15:01:09 +1000

    complete create a post page ui

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -21,9 +21,10 @@ import {
 } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
 import { HiOutlineExclamationCircle } from "react-icons/hi";
+import { Link } from "react-router-dom";
 
 export default function DashProfile() {
-    const { currentUser, error } = useSelector((state) => state.user);
+    const { currentUser, error, loading } = useSelector((state) => state.user);
     const [imageFile, setImageFile] = useState(null);
     const [imageFileUrl, setImageFileUrl] = useState(null);
     const [imageFileUploadProgress, setImageFileUploadProgress] =
@@ -142,19 +143,19 @@ export default function DashProfile() {
 
     const handleSignout = async () => {
         try {
-            const res = await fetch('api/user/signout', {
-                method: 'POST'
+            const res = await fetch("api/user/signout", {
+                method: "POST",
             });
             const data = await res.json();
-            if (!res.ok){
+            if (!res.ok) {
                 console.log(data.message);
-            } else{
+            } else {
                 dispatch(signoutSuccess());
             }
         } catch (error) {
             console.log(error);
         }
-    }
+    };
 
     return (
         <div className="max-w-lg mx-auto p-3 w-full">
@@ -226,9 +227,25 @@ export default function DashProfile() {
                     placeholder="password"
                     onChange={handleChange}
                 />
-                <Button type="submit" gradientDuoTone="purpleToBlue" outline>
-                    Update
+                <Button
+                    type="submit"
+                    gradientDuoTone="purpleToBlue"
+                    outline
+                    disabled={loading || imageFileUploading}
+                >
+                    {loading ? "Loading..." : "Update"}
                 </Button>
+                {currentUser.isAdmin && (
+                    <Link to={"/create-post"}>
+                        <Button
+                            type="button"
+                            gradientDuoTone="purpleToPink"
+                            className="w-full"
+                        >
+                            Create a Post
+                        </Button>
+                    </Link>
+                )}
             </form>
             <div className="text-red-500 flex justify-between mt-5">
                 <span
@@ -237,7 +254,9 @@ export default function DashProfile() {
                 >
                     Delete Account
                 </span>
-                <span onClick={handleSignout} className="cursor-pointer">Sign Out</span>
+                <span onClick={handleSignout} className="cursor-pointer">
+                    Sign Out
+                </span>
             </div>
             <div>
                 {updateUserSuccess && (
