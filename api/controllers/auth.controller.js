18d7c71c635d9414f5e8e054b305118dab947488
Brean Julius Carbonilla <julius.carbonilla@gmail.com>
2024-09-28 13:15:38 +1000

    add admin functionality to user

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -48,7 +48,7 @@ export const signin = async (req, res, next) => {
             return next(errorHandler(400, "Invalid password"));
         }
 
-        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const token = jwt.sign({ id: validUser._id, isAdmin:validUser.isAdmin }, process.env.JWT_SECRET);
         const { password: pass, ...rest } = validUser._doc;
 
         res.status(200)
@@ -62,7 +62,7 @@ export const google = async (req, res, next) => {
     try {
         const user = await User.findOne({ email });
         if (user) {
-            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+            const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT_SECRET);
             const { password: pass, ...rest } = user._doc;
             res.status(200)
                 .cookie("access_token", token, { httpOnly: true })
@@ -79,7 +79,7 @@ export const google = async (req, res, next) => {
                 profilePicture: googlePhotoUrl,
             });
             await newUser.save();
-            const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+            const token = jwt.sign({ id: newUser._id, isAdmin: newUser.isAdmin }, process.env.JWT_SECRET);
             const { password: pass, ...rest } = newUser._doc;
             res.status(200)
                 .cookie("access_token", token, { httpOnly: true })
