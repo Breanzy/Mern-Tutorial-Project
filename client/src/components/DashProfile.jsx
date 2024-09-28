12340f3cc3a61be4bfe6d2089d3182c79c05a81a
Brean Julius Carbonilla <julius.carbonilla@gmail.com>
2024-09-28 11:42:09 +1000

    complete delete user account functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,4 +1,4 @@
-import { Alert, Button, TextInput } from "flowbite-react";
+import { Alert, Button, Modal, TextInput } from "flowbite-react";
 import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import {
@@ -14,11 +14,15 @@ import {
     updateStart,
     updateSuccess,
     updateFailure,
+    deleteUserStart,
+    deleteUserSuccess,
+    deleteUserFailure,
 } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
+import { HiOutlineExclamationCircle } from "react-icons/hi";
 
 export default function DashProfile() {
-    const { currentUser } = useSelector((state) => state.user);
+    const { currentUser, error } = useSelector((state) => state.user);
     const [imageFile, setImageFile] = useState(null);
     const [imageFileUrl, setImageFileUrl] = useState(null);
     const [imageFileUploadProgress, setImageFileUploadProgress] =
@@ -28,6 +32,7 @@ export default function DashProfile() {
     const [imageFileUploading, setImageFileUploading] = useState(false);
     const [updateUserSuccess, setUpdateUserSuccess] = useState(null);
     const [updateUserError, setUpdateUserError] = useState(null);
+    const [showModal, setShowModal] = useState(false);
     const filePickerRef = useRef();
     const dispatch = useDispatch();
     const handleImageChange = (e) => {
@@ -62,7 +67,7 @@ export default function DashProfile() {
                     "Could not upload image (File must be less than 20MB)"
                 );
                 setImageFileUploadProgress(null);
-                setImageFiie(null);
+                setImageFile(null);
                 setImageFileUrl(null);
                 setImageFileUploading(false);
             },
@@ -106,7 +111,6 @@ export default function DashProfile() {
             if (!res.ok) {
                 dispatch(updateFailure(data.message));
                 setUpdateUserError(data.message);
-                return;
             } else {
                 dispatch(updateSuccess(data));
                 setUpdateUserSuccess("User's profile updated successfully");
@@ -114,6 +118,24 @@ export default function DashProfile() {
         } catch (error) {
             dispatch(updateFailure(error.message));
             setUpdateUserError(error.message);
+        }
+    };
+
+    const handleDeleteUser = async () => {
+        setShowModal(false);
+        try {
+            dispatch(deleteUserStart());
+            const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+                method: "DELETE",
+            });
+            const data = await res.json();
+            if (!response.ok) {
+                dispatch(deleteUserFailure(data.message));
+            } else {
+                dispatch(deleteUserSuccess(data));
+            }
+        } catch (error) {
+            dispatch(deleteUserFailure(error.message));
         }
     };
 
@@ -126,7 +148,7 @@ export default function DashProfile() {
                     accept="image/*"
                     onChange={handleImageChange}
                     ref={filePickerRef}
-                    hidden="true"
+                    hidden
                 />
                 <div
                     className="relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full"
@@ -192,7 +214,12 @@ export default function DashProfile() {
                 </Button>
             </form>
             <div className="text-red-500 flex justify-between mt-5">
-                <span className="cursor-pointer">Delete Account</span>
+                <span
+                    onClick={() => setShowModal(true)}
+                    className="cursor-pointer"
+                >
+                    Delete Account
+                </span>
                 <span className="cursor-pointer">Sign Out</span>
             </div>
             <div>
@@ -206,6 +233,41 @@ export default function DashProfile() {
                         {updateUserError}
                     </Alert>
                 )}
+                {error && (
+                    <Alert color="failure" className="mt-5">
+                        {error}
+                    </Alert>
+                )}
+                <Modal
+                    show={showModal}
+                    onClose={() => setShowModal(false)}
+                    popup
+                    size="md"
+                >
+                    <Modal.Header />
+                    <Modal.Body>
+                        <div className="text-center">
+                            <HiOutlineExclamationCircle className="h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto" />
+                            <h3 className="mb-5 text-lg text-gray-500 dark:text-gray-400">
+                                Are you sure you want to delete your account?
+                            </h3>
+                            <div className="flex justify-center gap-4">
+                                <Button
+                                    color="failure"
+                                    onClick={handleDeleteUser}
+                                >
+                                    Yes, I'm sure
+                                </Button>
+                                <Button
+                                    color="gray"
+                                    onClick={() => setShowModal(false)}
+                                >
+                                    No, cancel
+                                </Button>
+                            </div>
+                        </div>
+                    </Modal.Body>
+                </Modal>
             </div>
         </div>
     );
