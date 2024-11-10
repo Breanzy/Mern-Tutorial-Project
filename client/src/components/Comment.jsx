0392a03c0fa3d88b7fd78f890960f4d8cb7d38ae
Brean Julius Carbonilla <julius.carbonilla@gmail.com>
2024-11-10 21:54:02 +1100

    add delete functionality to the comment section

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -3,7 +3,7 @@ import moment from "moment";
 import { FaThumbsUp } from "react-icons/fa";
 import { useSelector } from "react-redux";
 import { Button, Textarea } from "flowbite-react";
-export default function Comment({ comment, onLike, onEdit }) {
+export default function Comment({ comment, onLike, onEdit, onDelete }) {
     const [user, setUser] = useState({});
     const [isEditing, setIsEditing] = useState(false);
     const [editedContent, setEditedContent] = useState(comment.content);
@@ -120,13 +120,24 @@ export default function Comment({ comment, onLike, onEdit }) {
                             {currentUser &&
                                 (currentUser._id === comment.userId ||
                                     currentUser.isAdmin) && (
-                                    <button
-                                        type="button"
-                                        onClick={handleEdit}
-                                        className="text-gray-400 hover:text-blue-500"
-                                    >
-                                        Edit
-                                    </button>
+                                    <>
+                                        <button
+                                            type="button"
+                                            onClick={handleEdit}
+                                            className="text-gray-400 hover:text-blue-500"
+                                        >
+                                            Edit
+                                        </button>
+                                        <button
+                                            type="button"
+                                            onClick={() =>
+                                                onDelete(comment._id)
+                                            }
+                                            className="text-gray-400 hover:text-red-500"
+                                        >
+                                            Delete
+                                        </button>
+                                    </>
                                 )}
                         </div>
                     </>
