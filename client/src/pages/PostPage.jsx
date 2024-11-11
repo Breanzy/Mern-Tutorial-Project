4c582eddd757c71b29566cb9c09dd7e4f64a36f5
Brean Julius Carbonilla <julius.carbonilla@gmail.com>
2024-11-11 21:53:49 +1100

    add recent article section to the post page

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -3,12 +3,14 @@ import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import CallToAction from "../components/CallToAction";
 import CommentSection from "../components/CommentSection";
+import PostCard from "../components/PostCard";
 
 export default function PostPage() {
     const { postSlug } = useParams();
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
     const [post, setPost] = useState(null);
+    const [recentPosts, setRecentPosts] = useState(null);
 
     useEffect(() => {
         const fetchPost = async () => {
@@ -32,6 +34,21 @@ export default function PostPage() {
             }
         };
         fetchPost();
+    }, [postSlug]);
+
+    useEffect(() => {
+        try {
+            const fetchRecentPosts = async () => {
+                const res = await fetch(`/api/post/getposts?limit=3`);
+                const data = await res.json();
+                if (res.ok) {
+                    setRecentPosts(data.posts);
+                }
+            };
+            fetchRecentPosts();
+        } catch (error) {
+            console.log(error.message);
+        }
     }, [postSlug]);
 
     if (loading)
@@ -70,13 +87,21 @@ export default function PostPage() {
             <div
                 className="p-3 max-w-2xl mx-auto w-full post-content"
                 dangerouslySetInnerHTML={{ __html: post && post.content }}
-            >
-                
-            </div>
+            ></div>
             <div className="max-w-4xl mx-auto w-full">
-                <CallToAction/>
+                <CallToAction />
             </div>
-            <CommentSection postId={post._id}/>
+            <CommentSection postId={post._id} />
+
+            <div className="flex flex-col justify-center items-center mb-5">
+                <h1 className="text-xl mt-5">Recent articles</h1>
+                <div className="flex-wrap gap-5 mt-5 justify-center">
+                    {recentPosts &&
+                        recentPosts.map((post) => (
+                            <PostCard key={post._id} post={post} />
+                        ))}
+                </div>
+            </div>
         </main>
     );
 }
