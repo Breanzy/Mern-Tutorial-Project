b54be050858a589a7098334fe68a63a4ea935ccd
Brean Julius Carbonilla <julius.carbonilla@gmail.com>
2024-11-18 14:18:52 +1100

    Added .DS_Store to gitignore & fix routing issues with node

--- api/index.js
+++ api/index.js
@@ -35,7 +35,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
 
-app.use(express.static(path.join(__dirname, "/clients/dist")));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
