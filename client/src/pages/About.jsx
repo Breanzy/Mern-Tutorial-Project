dbcac2589966fbbfab236ee2826718f055d2b0d1
Brean Julius Carbonilla <julius.carbonilla@gmail.com>
2024-11-18 11:56:58 +1100

    Complete about and projects

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,37 @@
 export default function About() {
-  return (
-    <div>About</div>
-  )
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+            <div className="max-w-2xl mx-auto p-3 text-center">
+                <div>
+                    <h1 className="text-3xl font-semibold font text-center my-7">
+                        About Breanzy's Blog
+                    </h1>
+                    <div className="text-md text-gray-500 flex flex-col gap-6">
+                        <p>
+                            Breanzy's Blog is a personal blog created by
+                            Breanzy. It is a place where I share my thoughts,
+                            experiences, and knowledge on various topics,
+                            including technology, programming, and personal
+                            development.
+                        </p>
+
+                        <p>
+                            My goal is to provide high-quality content that is
+                            engaging, informative, and educational. I strive to
+                            inspire and motivate others to learn and grow in
+                            their journeys.
+                        </p>
+
+                        <p>
+                            I am passionate about technology and programming,
+                            and I enjoy sharing my knowledge and experiences
+                            with others. I believe that technology has the power
+                            to transform lives and make the world a better place
+                            for everyone
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,5 +1,14 @@
+import CallToAction from "../components/CallToAction";
+
 export default function Projects() {
-  return (
-    <div>Projects</div>
-  )
+    return (
+        <div className="min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3">
+            <h1 className="text-3xl font-semibold">Projects</h1>
+            <p className="text-md text-gray-500">
+                This is a list of projects I've worked on. You can click on each
+                project
+            </p>
+            <CallToAction />
+        </div>
+    );
 }
