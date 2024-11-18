b0fc2135f701cb77ebf9a44741a3792f6de0e287
Brean Julius Carbonilla <julius.carbonilla@gmail.com>
2024-11-18 12:34:15 +1100

    add pre installation

--- api/index.js
+++ api/index.js
@@ -6,13 +6,20 @@ import authRoutes from "./routes/auth.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 
 mongoose
     .connect(process.env.MONGO)
-    .then(() => console.log("Connected to MongoDB"))
-    .catch((err) => console.error(err));
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -27,6 +34,11 @@ app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
+
+app.use(express.static(path.join(__dirname, "/clients/dist")));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
